346ef1d06092bce3f6aa6a036eb02c1a58f3e3f6
Clément Courageux <clementcourageuxsudan@gmail.com>
2020-03-27 15:34:50 +0100

    numbers correction first version

--- controllers/numbersController.js
+++ controllers/numbersController.js
@@ -1,5 +1,8 @@
 const validator = require('express-validator');
 var async = require('async');
+var crypto = require('crypto');
+
+// rng using seed (not available with math.random)
 const { Random, MersenneTwister19937 } = require("random-js");
 
 
@@ -8,7 +11,9 @@ exports.number_create_get = function(req, res) {
     res.render('numbers_form', {title: 'Start a Number Game'});
 };
 
+// when user submits the form to create a game
 exports.number_create_post = [
+    // parse parameters
     validator.body('amount', 'Amount of number required').trim().isLength({min: 1}),
     validator.body('amount', 'Amount must be a number').isNumeric(),
     validator.sanitizeBody('amount').escape(),
@@ -16,6 +21,12 @@ exports.number_create_post = [
     validator.body('group_by', 'Group_by must be a number').isNumeric(),
     validator.sanitizeBody('group_by').escape(),
 
+    validator.body('duration', 'Duration must be a number').isNumeric(),
+    validator.sanitizeBody('duration').escape(),
+
+    validator.sanitizeBody('seed').escape(),
+
+    // create the game or show errors
     (req, res, next) => {
         const err = validator.validationResult(req);
 
@@ -26,21 +37,48 @@ exports.number_create_post = [
             })
             return;
         }else {
+            var seed;
+
+            // create random seed if not provided
+            if(req.body.seed)
+                seed = req.body.seed;
+            else
+                seed = randU32Sync();
+
             res.render('number_play', {
                 title: 'Play Numbers', 
-                number_list: get_number_list_from_seed(req.body.amount, req.body.group_by)
+                number_list: get_number_list_from_seed(MersenneTwister19937.seed(seed), req.body.amount, req.body.group_by),
+                timer: req.body.duration*60,
+                seed:seed,
+                size:req.body.amount*req.body.group_by,
+                row:req.body.group_by
             });
         }
     }
 ];
 
+/** Perform correction after user's recall validation */
 exports.number_verify = function(req, res) {
-
+    var err=""
+    
+    if(! req.body.seed || ! req.body.size)
+        err="Play a game before verifying";
+    else {
+        res.render('numbers_verify',{
+            title:'Validate your recall:',
+            nbLines:req.body.row,
+            lenLine: req.body.size/req.body.row,
+            seed:req.body.seed,
+            size: req.body.size,
+            recall: req.body.recall,
+            correct: get_number_list_from_seed(MersenneTwister19937.seed(req.body.seed), req.body.size, 1),
+            err:err});
+    }
 }
 
 /** create random numbers to memorize from a seed */
-function get_number_list_from_seed(nLine, lSize) {
-    const random = new Random(MersenneTwister19937.autoSeed());
+function get_number_list_from_seed(seed, nLine, lSize) {
+    const random = new Random(seed);
     var nList = []
     for(var i=0; i<nLine; i++){
         nList.push([]);
@@ -49,4 +87,9 @@ function get_number_list_from_seed(nLine, lSize) {
         }
     }
     return nList;
+}
+
+// create a random 32bit int (https://stackoverflow.com/questions/28061016/generate-random-32-bit-number-in-node)
+function randU32Sync() {
+  return crypto.randomBytes(4).readUInt32BE(0, true);
 }
--- routes/numbers.js
+++ routes/numbers.js
@@ -3,9 +3,12 @@ var router = express.Router();
 
 var number_controller = require('../controllers/numbersController');
 
-// index page for games
-//router.get('/play', number_controller.number_play);
+// create a new number game
 router.get('/create', number_controller.number_create_get);
 router.post('/create', number_controller.number_create_post);
+
+// verify a played game
+router.post('/verify', number_controller.number_verify);
+
 
 module.exports = router;
